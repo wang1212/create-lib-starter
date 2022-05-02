c613e26e1c5fbace4f88f64e26171ed1cd6455a5
wang1212 <mrwang1212@126.com>
2022-05-02 13:42:07 +0800

    feat(ts): upgrade eslint to v8 :arrow_up:

---
+++ templates/typescript/.eslintrc.cjs
@@ -0,0 +1,21 @@
+// see docs: https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  extends: ['@wang1212/eslint-config/typescript'],
+  ignorePatterns: ['node_modules', '.husky', '.github', 'build', 'types'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    jest: false,
+  },
+  plugins: [],
+  settings: {},
+  rules: {},
+  globals: {},
+};
